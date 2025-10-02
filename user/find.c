f0dd8b9abe85e700ad147f24f7d7214cd5a63bba
Qiumix <j487428862@gmail.com>
2025-10-02 13:00:10 +0800

    [WIP] lab util/find

--- user/find.c
+++ user/find.c
@@ -1,7 +1,56 @@
 #include "kernel/types.h"
+#include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+void find(char *path, char *name) {
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(path, O_RDONLY)) < 0) {
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    close(fd);
+    return;
+  }
+
+  switch (st.type) {
+  case T_DEVICE:
+  case T_FILE:
+    break;
+
+  case T_DIR:
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+      break;
+    }
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if (stat(buf, &st) < 0) {
+        printf("ls: cannot stat %s\n", buf);
+        continue;
+      }
+      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, (int)st.size);
+    }
+    break;
+  }
+  close(fd);
+}
 int main(int argc, char **argv) {
-  //
+  if (argc != 2) {
+    exit(1);
+  }
+  char *PWD = ".";
+  find(PWD, argv[1]);
+  return 0;
 }
