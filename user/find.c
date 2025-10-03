de699a64cc1b9a0f464180119dc8817e9c7ccca1
Qiumix <j487428862@gmail.com>
2025-10-03 12:08:16 +0800

    complete find

--- user/find.c
+++ user/find.c
@@ -4,6 +4,7 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 #define BUF_SIZE 512
+
 char *fmtname(char *path) {
   static char buf[DIRSIZ + 1];
   char *p;
@@ -21,19 +22,87 @@ char *fmtname(char *path) {
   return buf;
 }
 
-// char *re_build_pre(char *pre, char *cur) {
-//   static char buf[BUF_SIZE];
-//   int pre_len = strlen(pre);
-//   int cur_len = strlen(cur);
-//   if (pre_len + cur_len > BUF_SIZE) {
-//     return pre;
-//   }
-//   strcpy(buf, pre);
-//   buf[pre_len] = '/';
-//   strcpy(buf + pre_len + 1, cur);
-//   return buf;
-// }
+void find_exec(char *path, char *name, int argc, char **argv) {
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
 
+  if ((fd = open(path, O_RDONLY)) < 0) {
+    fprintf(2, "find: failed to open %s\n", path);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "find: failed to stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  if (read(fd, &de, sizeof(de)) != sizeof(de)) {
+    exit(1);
+  }
+
+  switch (st.type) {
+  case T_FILE:
+    if (strcmp(name, de.name) == 0) {
+      int pid = fork();
+      if (pid == 0) {
+        char *temp[20];
+        for (int i = 0; i < argc; i++) {
+          temp[i] = argv[i];
+        }
+        temp[argc] = path;
+        temp[argc + 1] = name;
+        temp[argc + 2] = 0;
+        exec(argv[0], temp);
+      } else {
+        wait((int *)0);
+      }
+    }
+    break;
+
+  case T_DIR:
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+      fprintf(2, "find: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+      if (de.inum == 0 || strcmp(".", de.name) == 0 ||
+          strcmp("..", de.name) == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if (stat(buf, &st) < 0) {
+        fprintf(2, "find: cannot stat %s\n", buf);
+        continue;
+      }
+      if (st.type == T_FILE) {
+        if (strcmp(name, de.name) == 0) {
+          int pid = fork();
+          if (pid == 0) {
+            char *temp[20];
+            for (int i = 0; i < argc; i++) {
+              temp[i] = argv[i];
+            }
+            temp[argc] = buf;
+            temp[argc + 1] = 0;
+            exec(argv[0], temp);
+          } else {
+            wait((int *)0);
+          }
+        }
+      } else if (st.type == T_DIR) {
+        find_exec(buf, name, argc, argv);
+      }
+    }
+    break;
+  }
+  close(fd);
+}
 void find(char *path, char *name) {
   char buf[512], *p;
   int fd;
@@ -92,13 +161,18 @@ void find(char *path, char *name) {
   }
   close(fd);
 }
+
 int main(int argc, char **argv) {
-  if (argc != 3) {
+  if (argc < 3) {
     fprintf(2, "usage: find [dirname] [filename]");
     exit(1);
   }
   char *path = argv[1];
   char *filename = argv[2];
-  find(path, filename);
+  if (argc == 3) {
+    find(path, filename);
+  } else if (strcmp(argv[3], "-exec") == 0 && argc > 4) {
+    find_exec(path, filename, argc - 4, argv + 4);
+  }
   exit(0);
 }
